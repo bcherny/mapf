2a8b22dac32c9527b6d2b671592987478a98c68f
Boris Cherny <boris@performancejs.com>
2017-01-31 13:47:06 -0800

    add index and array to callback

--- index.ts
+++ index.ts
@@ -3,6 +3,6 @@
  *
  * `const [a, b, c] = await mapf([1, 2, 3], async _ => await foo(_) )`
  */
-export function mapf<T, U, V>(array: T[], cb: (this: V, t: T) => Promise<U>, thisArg?: V): Promise<U[]> {
+export function mapf<T, U, V>(array: T[], cb: (this: V, t: T, index: number, array: T[]) => Promise<U>, thisArg?: V): Promise<U[]> {
   return Promise.all<U>(array.map(cb, thisArg))
 }
